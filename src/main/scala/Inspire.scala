8ea9a478f6aea3817a0e756e4ec433b2131b18b8
unknown <basil@.(none)>
2012-03-18 10:18:31 +0700

    Bibtex pasing and inspire import for journal articles now somehow works.

--- src/main/scala/Inspire.scala
+++ src/main/scala/Inspire.scala
@@ -27,6 +27,16 @@ class Inspire(val author:Author, year:Int) {
 	}
 	//Use inside transaction
 	def run {
+		saveArticles(getBibtex)
+	}
+	def saveArticles(publications:Iterable[Publication]) {
+		for(p <- publications) {
+			val saved = p.findOrInsert
+			saved.authors.dissociate(author)
+			saved.authors.associate(author)
+		}
+	}
+	def getBibtex = {
 		var uri = new URI("http", "inspirehep.net", "/search", "p=author:\""+author.inspireName+"\"+AND+date:\""+year+"\"&of=hx&rg=200","")
 		println(uri)
 		val stream = new FileInputStream(new File("search.htm"))
@@ -34,34 +44,37 @@ class Inspire(val author:Author, year:Int) {
 		val parserFactory = new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 		val parser = parserFactory.newSAXParser()
 		val adapter = new scala.xml.parsing.NoBindingFactoryAdapter
-		processBibtex(adapter.loadXML(new InputSource(stream), parser))
+		val xml = adapter.loadXML(new InputSource(stream), parser)
+		val nodes:NodeSeq = (xml \\ "pre")
+		nodes.map(bibTexNodeToPublication).flatten
 	}
 	def fieldToPair(field:String) = {
 		val pair = field.split("=", 2)
 		if (pair.length < 2) 
 			throw new RuntimeException("Can't split by = :"+field)
 		val name = pair(0).trim();
-		val value = pair(1).trim().replaceAll("^\"+", "").replaceAll("\n +", "")
+		val value = pair(1).replaceAll("[\n ]+", " ").trim().replaceAll("^\"|\"$", "");
 		(name, value)
 	}
 	def parseHTML(data:String) = {
 		scala.xml.Text(data).text
 	}
-	def bibTexNodeToPublication(elem:Node) = {
+	def bibTexNodeToPublication(elem:Node):Iterable[Publication] = {
 		val sections = elem.text.split("\",\n +")
 		val fields = (sections filter (_.length > 4) map fieldToPair).toMap
 		println(fields("SLACcitation"))
 		println(fields("journal")+fields("volume"))
 		val publisher = guessJournal(fields("journal"), fields("volume"))
 		val year = fields("year").toInt
-		val title = parseHTML(fields("title").replaceAll("^\\{|\\}$", "")).replaceAll("&gt;", ">");
+		val raw = fields("title")
+		println("Raw: "+raw)
+		val parsed1 = parseHTML(raw)
+		println("Parsed1: "+parsed1)
+		val stripped = parsed1.replaceAll("^\\{|\\}$", "")
+		println("Stripped: "+stripped)
+		val title = stripped.replaceAll("&gt;", ">");
 		println(title)
-		val p = new Publication(publisher.id, 100, year, title).findOrInsert
-		p.authors.associate(author)
-		p
-	}
-	def processBibtex(data:NodeSeq): Iterable[Publication] = {
-		val nodes:NodeSeq = (data \\ "pre")
-		nodes.map(bibTexNodeToPublication)
+		val p = new Publication(publisher.id, 100, year, title)
+		Seq(p)
 	}
 }
